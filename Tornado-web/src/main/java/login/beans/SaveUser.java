bf3835fb7fcb670b2df0f61c6d2d393b239ffef9
Johan David Rodriguez Portela <jodrodriguezpo@unal.edu.co>
2014-05-24 21:40:48 -0500

    Este commit va hasta la creacion de usuarios con rol desde el sitio web

--- Tornado-web/src/main/java/login/beans/SaveUser.java
+++ Tornado-web/src/main/java/login/beans/SaveUser.java
@@ -8,6 +8,7 @@ package login.beans;
 import edu.unal.dto.UserDTO;
 import edu.unal.factory.ServiceFactory;
 import edu.unal.helper.HashSHA256;
+import edu.unal.model.Rol;
 import edu.unal.services.UserService;
 import java.awt.event.ActionEvent;
 import javax.faces.application.FacesMessage;
@@ -27,15 +28,30 @@ public class SaveUser {
     private final UserService userService;
     private String name;
     private String password;
+    private Rol rol;
 
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public void setRoles(Rol[] roles) {
+        this.roles = roles;
+    }
+    private Rol roles[]=new Rol[2];
+    
     public SaveUser() {
         ServiceFactory sf=ServiceFactory.getInstance();
         this.userService=sf.getUserService();
+        this.setFixedRoles();
     }
     
     public void saveUser(ActionEvent actionEvent) {
         FacesContext context = FacesContext.getCurrentInstance();
-        UserDTO dto = new UserDTO(name, password);
+        UserDTO dto = new UserDTO(name, password,rol);
         userService.save(dto);
         context.addMessage(null, new FacesMessage("Almacenado en la base de datos"+dto.toString()));
     }
@@ -55,11 +71,14 @@ public class SaveUser {
     public void setPassword(String password) {
         HashSHA256 hash=new HashSHA256();
         this.password = hash.getHash(password);
-    }
-    
-    
-    
-    
-    
+    }           
 
+    public void setFixedRoles(){
+        roles[0]=rol.ADMINISTRADOR;
+        roles[1]=rol.DISENADOR;
+    }
+
+    public Rol[] getRoles() {
+        return roles;
+    }
 }
