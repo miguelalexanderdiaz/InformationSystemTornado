f180ead61b1d439f3a8387f012b518d73acb5786
migueldiaz <migueldiaz@migueldiaz-ThinkPad-X220>
2014-05-28 18:05:02 -0500

    avance inventario

---
+++ Tornado-web/src/main/java/edu/unal/user/ui/InventoryBean.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.unal.user.ui;
+
+import java.util.ArrayList;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author migueldiaz
+ */
+@ManagedBean
+@RequestScoped
+public class InventoryBean {
+    
+   // private ArrayList<>
+
+    /**
+     * Creates a new instance of InventoryBean
+     */
+    public InventoryBean() {
+    }
+    
+}
